74877fb7c6982e62e426221b2767b47f91094f51
elbaih101 <l.ofwarhd@gmail.com>
2024-03-07 11:11:25 +0200

    batch file for jmeter run

---
+++ src/main/Performance test Plan/stage checked out reservations.bat
@@ -0,0 +1,13 @@
+@echo off
+set name=reservation-cycle
+set dashBoardDataLocation="F:\java maven projects\Nazeel-Project\src\main\Performance test Plan\dashboarddata\%name%\"
+set dashBoard="F:\java maven projects\Nazeel-Project\src\main\Performance test Plan\dash-board\%name%\"
+set csvLog="%dashBoardDataLocation%log.csv"
+set runLog="%dashBoardDataLocation%runlog.txt"
+set jmxFile ="F:\java maven projects\Nazeel-Project\src\main\Performance test Plan\load_Testing.jmx"
+Echo starting reservation cycle load test
+Echo changing directory to jmeter bin
+call cd "F:\java maven projects\Nazeel-Project\src\main\Performance test Plan\apache-jmeter-5.6.2\bin"
+Echo running the test
+call jmeter -n -t "%jmxFile%" -l "%csvLog%" -j "%runLog%" -e -o "%dashBoard%"
+ECHO test completed
