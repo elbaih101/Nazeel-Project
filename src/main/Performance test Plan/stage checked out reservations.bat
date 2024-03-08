2b855f534ccde8ffe299ef2859ac3b070cc2f41e
elbaih101 <l.ofwarhd@gmail.com>
2024-03-08 19:20:38 +0200

    fixing the batch file command for jmeter run

--- src/main/Performance test Plan/stage checked out reservations.bat
+++ src/main/Performance test Plan/stage checked out reservations.bat
@@ -1,13 +1,18 @@
 @echo off
+setlocal
 set name=reservation-cycle
-set dashBoardDataLocation="F:\java maven projects\Nazeel-Project\src\main\Performance test Plan\dashboarddata\%name%\"
-set dashBoard="F:\java maven projects\Nazeel-Project\src\main\Performance test Plan\dash-board\%name%\"
-set csvLog="%dashBoardDataLocation%log.csv"
-set runLog="%dashBoardDataLocation%runlog.txt"
-set jmxFile ="F:\java maven projects\Nazeel-Project\src\main\Performance test Plan\load_Testing.jmx"
+set dashBoardDataLocation=D:\java maven projects\Nazeel-Project\src\main\Performance test Plan\dashboarddata\%name%
+set dashBoard=D:\java maven projects\Nazeel-Project\src\main\Performance test Plan\dash-board\%name%
+set csvLog=%dashBoardDataLocation%\log.csv
+set runLog=%dashBoardDataLocation%\runlog.txt
+set jmxFile=D:\java maven projects\Nazeel-Project\src\main\Performance test Plan\load_Testing.jmx
 Echo starting reservation cycle load test
 Echo changing directory to jmeter bin
-call cd "F:\java maven projects\Nazeel-Project\src\main\Performance test Plan\apache-jmeter-5.6.2\bin"
+Echo "%jmxFile%"
+Echo "%csvLog%"
+Echo "%runLog%"
+Echo "%dashBoard%"
+call cd "D:\java maven projects\Nazeel-Project\src\main\Performance test Plan\apache-jmeter-5.6.3\bin"
 Echo running the test
-call jmeter -n -t "%jmxFile%" -l "%csvLog%" -j "%runLog%" -e -o "%dashBoard%"
+call jmeter -n  -t "%jmxFile%" -l "%csvLog%" -j "%runLog%" -e -o "%dashBoard%"
 ECHO test completed
