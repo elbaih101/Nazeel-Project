0c8330d05dd668830cb2ea5ec07390f6b4f64aaa
elbaih101 <l.ofwarhd@gmail.com>
2024-03-18 11:17:13 +0200

    - fiddling around with the batch file to enable distributed load testing

--- src/main/Performance test Plan/stage checked out reservations.bat
+++ src/main/Performance test Plan/stage checked out reservations.bat
@@ -5,7 +5,7 @@ set dashBoardDataLocation=F:\java maven projects\Nazeel-Project\src\main\Perform
 set dashBoard=F:\java maven projects\Nazeel-Project\src\main\Performance test Plan\dash-board\%name%
 set csvLog=%dashBoardDataLocation%\log.csv
 set runLog=%dashBoardDataLocation%\runlog.txt
-set jmxFile=F:\java maven projects\Nazeel-Project\src\main\Performance test Plan\load_Testing.jmx
+set jmxFile=F:\java maven projects\Nazeel-Project\src\main\Performance test Plan\Reservations full Cycle.jmx
 set "distributed=-Gremote_hosts=127.0.0.1,192.168.100.135"
 Echo starting reservation cycle load test
 Echo changing directory to jmeter bin
@@ -15,5 +15,5 @@ Echo "%runLog%"
 Echo "%dashBoard%"
 call cd "F:\java maven projects\Nazeel-Project\src\main\Performance test Plan\apache-jmeter-5.6.2\bin"
 Echo running the test
-call jmeter -n  -t "%jmxFile%" -l "%csvLog%" -j "%runLog%" -e -o "%dashBoard%" -Gremote_hosts=127.0.0.1
+call jmeter -n  -t "%jmxFile%" -l "%csvLog%" -j "%runLog%" -e -o "%dashBoard%" %distributed%
 ECHO test completed
