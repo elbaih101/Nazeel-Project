259f8f251c8fe90edb80a47ab0684810e19ff8ab
elbaih101 <l.ofwarhd@gmail.com>
2024-04-23 16:23:27 +0200

    -rent taxes and discounts++outlet orders

--- src/main/java/org/example/Utils.java
+++ src/main/java/org/example/Utils.java
@@ -205,4 +205,9 @@ public class Utils {
         Thread.sleep(500);
         driver.switchTo().window(winHandles.get(tabIndex));
     }
+
+    public static double round(double value, int places) {
+        double scale = Math.pow(10, places);
+        return Math.round(value * scale) / scale;
+    }
 }
