349d2a4b9510c966e70dc687edd8eb4810c40ac7
elbaih101 <l.ofwarhd@gmail.com>
2024-05-26 22:04:03 +0300

    code clean up again

--- src/main/java/alia/nazeel/tools/CustomWebDriverWait.java
+++ src/main/java/alia/nazeel/tools/CustomWebDriverWait.java
@@ -30,16 +30,17 @@ public class CustomWebDriverWait extends WebDriverWait
     }
 
     public void waitLoading() {
+        WebDriver driver =DriverManager.getDriver();
         try {
-           DriverManager.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+           driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
             // Wait until the loading animation disappears or becomes stale
             this.withTimeout(Duration.ofSeconds(20))
                     .ignoring(NoSuchElementException.class, StaleElementReferenceException.class)
-                    .until(ExpectedConditions.invisibilityOf( DriverManager.getDriver().findElement(By.xpath("//app-loading-page/*"))));
+                    .until(ExpectedConditions.invisibilityOf( driver.findElement(By.xpath("//app-loading-page/*"))));
         } catch (Exception e) {
             // Handle any exceptions or logging here
             e.printStackTrace();
         }
-        DriverManager.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 }
