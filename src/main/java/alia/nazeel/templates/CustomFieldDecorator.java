4988b71dcf51763a5867cd382c39486f8a83d971
elbaih101 <l.ofwarhd@gmail.com>
2024-06-02 21:49:23 +0300

    Refactored Custom Field Decorator

--- src/main/java/alia/nazeel/templates/CustomFieldDecorator.java
+++ src/main/java/alia/nazeel/templates/CustomFieldDecorator.java
@@ -6,8 +6,14 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.pagefactory.DefaultFieldDecorator;
 import org.openqa.selenium.support.pagefactory.ElementLocator;
 import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * this Class Extends the custom Field Decorator then can be used inside the BasePom in replace of the Default Field decorator
@@ -16,6 +22,22 @@ import java.lang.reflect.Field;
  */
 public class CustomFieldDecorator extends DefaultFieldDecorator
 {
+    private static final Logger logger = LoggerFactory.getLogger(CustomFieldDecorator.class);
+    // Just add the new Custom Web Element Class to this List and it Should work like charm
+    //Note The Custom WebElement Should have a constructor that takes a WebElement
+    List<Class<?>> elements = Collections.unmodifiableList(new ArrayList<>()
+    {{
+        add(KendoGrid.class);
+        add(KendoDateTimePicker.class);
+        add(KendoButtonGroup.class);
+        add(KendoComboBox.class);
+        add(ChipsMultiselect.class);
+        add(KendoGrid.class);
+        add(CustomWebElement.class);
+        add(KendoGrid.class);
+
+    }});
+
 
     public CustomFieldDecorator(ElementLocatorFactory factory)
     {
@@ -36,160 +58,44 @@ public class CustomFieldDecorator extends DefaultFieldDecorator
             /*
              * Here We enter the required new Custom Elements and Then Get their proxies
              **/
-            if (field.getType().equals(CustomWebElement.class))
+            for (Class<?> element : elements)
             {
-                return proxyForCustomWebElement(loader, locator);
-            } else if (field.getType().equals(KendoDateTimePicker.class))
-            {
-                return proxyForKendoDateTimePicker(loader, locator);
-            } else if (field.getType().equals(KendoButtonGroup.class))
-            {
-                return proxyForKendoButtonGroup(loader, locator);
-            } else if (field.getType().equals(KendoComboBox.class))
-            {
-                return proxyForKendoComboBox(loader, locator);
-            } else if (field.getType().equals(ChipsMultiselect.class))
-            {
-                return proxyForChipsMultiselect(loader, locator);
-            }else if (field.getType().equals(KendoGrid.class))
-            {
-                return proxyForKendoGrid(loader, locator);
-            }else if (field.getType().equals(KendoDropDownList.class))
-            {
-                return proxyForKendoDropDownList(loader, locator);
-            }else if (field.getType().equals(MultiLangTextField.class))
-            {
-                return proxyForMultiLangTextField(loader, locator);
-            }else if (field.getType().equals(KendoMultiSelect.class))
-            {
-                return proxyForKendoMultiSelect(loader, locator);
-            } else if (field.getType().equals(WebElement.class))
-            {
-                return proxyForWebElement(loader, locator);
+                if (field.getType().equals(element))
+                {
+                    return CustomProxy(element, loader, locator);
+                }
             }
         }
         return super.decorate(loader, field);
     }
-
-    private Object proxyForMultiLangTextField(ClassLoader loader, ElementLocator locator) {
-        WebElement proxy =  proxyForLocator(loader, locator);
-        return new MultiLangTextField(proxy);
-    }
-
     /**
-     * Creates a KendoMultiSelect proxy for handling the annotations of Find By.
+     * This method creates a proxy instance of the specified class using the provided WebElement as an argument.
+     * It finds a constructor that takes a WebElement as an argument and creates a new instance using that constructor.
      *
-     * @param loader  the class loader handling the Pom Page
-     * @param locator the Locator Defined in the field of the find by annotation @FindBy(<Css="">)
-     * @return a new instance of KendoMultiSelect with the provided WebElement proxy
+     * @param clas  The class of the proxy instance to be created.
+     * @param loader The class loader to be used for loading the specified class.
+     * @param locator The locator that provides the WebElement to be used for creating the proxy instance.
+     * @return A new instance of the specified class, initialized with the provided WebElement.
+     * @see ElementLocator
+     * @see WebElement
      */
-    private Object proxyForKendoMultiSelect(ClassLoader loader, ElementLocator locator)
+    private <T> T CustomProxy(Class<T> clas, ClassLoader loader, ElementLocator locator)
     {
-        WebElement proxy =  proxyForLocator(loader, locator);
-        return new KendoMultiSelect(proxy);
-    }
+        try
+        {
+            WebElement proxy = proxyForLocator(loader, locator);
 
-    /**
- * Creates a KendoGrid proxy for handling the annotations of Find By.
- *
- * @param loader  the class loader handling the Pom Page
- * @param locator the Locator Defined in the field of the find by annotation @FindBy(<Css="">)
- * @return a new instance of KendoGrid with the provided WebElement proxy
- */
-private KendoGrid proxyForKendoGrid(ClassLoader loader, ElementLocator locator)
-{
-    WebElement proxy = proxyForLocator(loader, locator);
-    return new KendoGrid(proxy);
-}
+            // Find a constructor that takes a WebElement as an argument
+            Constructor<T> constructor = clas.getConstructor(WebElement.class);
 
-    /**
- * Creates a KendoDropDownList proxy for handling the annotations of Find By.
- *
- * @param loader  the class loader handling the Pom Page
- * @param locator the Locator Defined in the field of the find by annotation @FindBy(<Css="">)
- * @return a new instance of KendoDropDownList with the provided WebElement proxy
- */
-private KendoDropDownList proxyForKendoDropDownList(ClassLoader loader, ElementLocator locator)
-{
-    WebElement proxy =  proxyForLocator(loader, locator);
-    return new KendoDropDownList(proxy);
-}
-
-    /**
- * Creates a KendoComboBox proxy for handling the annotations of Find By.
- *
- * @param loader  the class loader handling the Pom Page
- * @param locator the Locator Defined in the field of the find by annotation @FindBy(<Css="">)
- * @return a new instance of KendoComboBox with the provided WebElement proxy
- */
-private KendoComboBox proxyForKendoComboBox(ClassLoader loader, ElementLocator locator)
-{
-    WebElement proxy =  proxyForLocator(loader, locator);
-    return new KendoComboBox(proxy);
-}
-
-    /**
- * Creates a KendoButtonGroup proxy for handling the annotations of Find By.
- *
- * @param loader  the class loader handling the Pom Page
- * @param locator the Locator Defined in the field of the find by annotation @FindBy(<Css="">)
- * @return a new instance of KendoButtonGroup with the provided WebElement proxy
- */
-private KendoButtonGroup proxyForKendoButtonGroup(ClassLoader loader, ElementLocator locator)
-{
-    WebElement proxy = proxyForLocator(loader, locator);
-    return new KendoButtonGroup(proxy);
-}
-
-    /**
- * Custom ChipsMultiselect Proxy For Handling the Annotations of Find By
- *
- * @param loader  the class loader handling the Pom Page
- * @param locator the Locator Defined in the field of the find by annotation @FindBy(<Css="">)
- * @return a new instance of ChipsMultiselect with the provided WebElement proxy
- */
-private ChipsMultiselect proxyForChipsMultiselect(ClassLoader loader, ElementLocator locator)
-{
-    WebElement proxy = proxyForLocator(loader, locator);
-    return new ChipsMultiselect(proxy);
-}
-
-    /**
-     * Custom WebElement Proxy For Handling the Annotations of Find By
-     *
-     * @param loader  the class loader handling the Pom Page
-     * @param locator the Locator Defined in the field of the find by annotation @FindBy(<Css="">)
-     * @return a custom Web element
-     */
-    private CustomWebElement proxyForCustomWebElement(ClassLoader loader, ElementLocator locator)
-    {
-        WebElement proxy =  proxyForLocator(loader, locator);
-        return new CustomWebElement(proxy);
-    }
-
-    /**
-     * Custom KendoDateTimePicker Proxy For Handling the Annotations of Find By
-     *
-     * @param loader  the class loader handling the Pom Page
-     * @param locator the Locator Defined in the field of the find by annotation @FindBy(<Css="">)
-     * @return a new instance of KendoDateTimePicker with the provided WebElement proxy
-     */
-    private KendoDateTimePicker proxyForKendoDateTimePicker(ClassLoader loader, ElementLocator locator)
-    {
-        WebElement proxy = proxyForLocator(loader, locator);
-        return new KendoDateTimePicker(proxy);
-    }
-
-    /**
-     * Creates a WebElement proxy for handling the annotations of Find By.
-     *
-     * @param loader  the class loader handling the Pom Page
-     * @param locator the Locator Defined in the field of the find by annotation @FindBy(<Css="">)
-     * @return a new instance of WebElement with the provided WebElement proxy
-     */
-    private WebElement proxyForWebElement(ClassLoader loader, ElementLocator locator)
-    {
-        return  proxyForLocator(loader, locator);
+            // Create a new instance using the found constructor
+            return constructor.newInstance(proxy);
+        } catch (Exception e)
+        {
+            // Handle exceptions such as NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException
+          logger.error(e.getMessage(), e);
+        throw new RuntimeException("Failed to create proxy instance", e);
+        }
     }
 }
 
